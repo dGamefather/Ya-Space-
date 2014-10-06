35672cee60ee929d10a6c83ac3730bb0b532143f
Brennan Litster <brennan@thatoneplace.net>
2014-10-06 13:42:16 -0600

    Added to GitHub

---
+++ src/ctec/yaspace/LameActivity.java
@@ -0,0 +1,20 @@
+package ctec.yaspace;
+
+import ctec.yaspace.R;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LameActivity extends ActionBarActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lame);
+		int x = 7;
+		x++;
+	}
+}
